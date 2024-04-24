deb29553f60a36c4b3f09d4420b27de3e270914e
FranciscoRosa-UCA <00046821@uca.edu.sv>
2024-04-23 18:15:19 -0600

    update: :lock: dto's restrictions

--- api/src/main/java/com/codigotruko/taller2/domain/dtos/SaveUserDTO.java
+++ api/src/main/java/com/codigotruko/taller2/domain/dtos/SaveUserDTO.java
@@ -1,12 +1,18 @@
 package com.codigotruko.taller2.domain.dtos;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class SaveUserDTO {
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
+    @Size(min=5, max = 20, message = "El nombre de usuario no puede tener más de 20 caracteres")
     private String username;
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "La contraseña debe tener al menos 8 caracteres, incluir mayúsculas, minúsculas, números y caracteres especiales")
     private String password;
+    @NotBlank(message = "El correo electrónico no puede estar vacío")
+    @Email(message = "El correo electrónico debe tener un formato válido")
     private String email;
 
 }
